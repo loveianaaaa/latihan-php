c19df732e3e3123648f6513c966c4287d6368d7e
loveianaaaa <2223602.loveiana@smkn-2sbg.sch.id>
2024-02-15 15:07:38 +0700

    latihan php

---
+++ 10_strings/main.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<body>
+<!DOCTYPE html>
+<html>
+<body>
+    <!-- identitas -->
+<style>
+    table {
+        border-collapse: collapse;
+    }
+    table, th, td {
+        border: 1px solid black;
+    }
+    th, td {
+        padding: 10px;
+        background-color: #f0c6ff;
+
+    }
+    th {
+        background-color: #00ff00;
+        color: white;
+    }
+  </style>
+  </head>
+  <body>
+  <table>
+    <tr>
+        <th>nama</th>
+        <th>kelas</th>
+        <th>tanggal praktikum</th>
+        <th>foto</th>
+    </tr>
+    <tr>
+        <td>loveiana</td>
+        <td>XI-RPL</td>
+        <td>30-januari-2024</td>
+        <td><img src="../poto opey.jpg" alt="Flowers in Chania" width="140" height="90"></img></td>
+    </tr>
+    <tr>          
+  </table>
+<?php
+echo "Hello";
+print "Hello";
+?>
+
+</body>
+</html>
